5291d00f1157bd3fda7d18f3e557f64b392604b5
Sumit0925 <smtangural@gmail.com>
2024-09-18 21:03:51 +0530

    9th commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import "./App.scss";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
+import Parallax from "./components/Parallax/Parallax";
 
 const App = () => {
   return (
@@ -11,9 +12,9 @@ const App = () => {
         <Navbar />
         <Hero/>
       </section>
-      <section id="Services">Parallax</section>
+      <section id="Services"><Parallax type="Services" /></section>
       <section>Services</section>
-      <section id="Portfolio">Parallax</section>
+      <section id="Portfolio"><Parallax type="Portfolio" /></section>
       <section>Portfolio1</section>
       <section>Portfolio2</section>
       <section>Portfolio3</section>
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,23 +1,81 @@
 import React from "react";
 import "./hero.scss";
+import { motion } from "framer-motion";
+
+const textVariants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
+
+const slidingVariant = {
+  initial: {
+    x: 0,
+  },
+  animate: {
+    x: "-400%",
+    transition: {
+      duration: 20,
+      repeat:Infinity,
+      repeatType:"mirror"
+      
+    },
+  },
+};
 
 const Hero = () => {
   return (
     <>
       <div className="hero">
         <div className="wrapper">
-        <div className="text-container">
-            <h2>SUMIT ANGURAL</h2>
-            <h1>Full stack Web Developer</h1>
-            <div className="buttons">
-                <button>See the Latest Works</button>
-                <button>Contact Me</button>
-            </div>
-            <img src="/scroll.png" alt="mouse-image" />
-        </div>
-        <div className="slidingTextContianer">
-          Web Developer
-        </div>
+          <motion.div
+            className="text-container"
+            variants={textVariants}
+            initial="initial"
+            animate="animate"
+          >
+            <motion.h2 variants={textVariants}>SUMIT ANGURAL</motion.h2>
+            <motion.h1 variants={textVariants}>
+              Full stack Web Developer
+            </motion.h1>
+            <motion.div className="buttons" variants={textVariants}>
+              <motion.button variants={textVariants}>
+                See the Latest Works
+              </motion.button>
+              <motion.button variants={textVariants}>Contact Me</motion.button>
+            </motion.div>
+            <motion.img
+              variants={textVariants}
+              animate="scrollButton"
+              src="/scroll.png"
+              alt="mouse-image"
+            />
+          </motion.div>
+          <motion.div
+            variants={slidingVariant}
+            initial="initial"
+            animate="animate"
+            className="slidingTextContianer"
+          >
+            Web Developer
+          </motion.div>
         </div>
         <div className="image-container">
           <img src="/hero.png" alt="hero-image" />
